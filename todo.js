e4d360f1004a1f767605c944ff8a5b3f889ad474
Grace C <51942103+GraceeC@users.noreply.github.com>
2019-10-03 07:36:28 -0700

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,16 +1,22 @@
+//adding a new LI element
 function add() {
   const inputToDo = document.getElementById('inputToDo');
   const li = document.createElement("li");
   const textNode = document.createTextNode(inputToDo.value);
   li.appendChild(textNode);
-  const box = document.createElement('checkBox');
-  box.document.getElementById('checkBox')
   document.getElementById("toDoList").appendChild(li);
-  box.appendChild(document.createTextNode('x'));
 }
+//creating a new checkbox
+function checkbox(){
+  const inputToDo = document.getElementById('inputToDo');
+  const ul = document.createElement(checkb);
+  const textNode = document.createTextNode(inputToDo.value);
+  ul.appendChild(textNode);
+  document.getElementById("toDoList").appendChild(li);
+}
+//removing li by checkbox
 function removeLi(){
  const removeLi = document.getElementById('toDoList');
  const parent = removeLi.parentNode;
  parent.removeChild(removeLi);
 }
-
