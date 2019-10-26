ca10b90c4b25894d089ec994a23f355b34bb035f
Grace C <51942103+GraceeC@users.noreply.github.com>
2019-10-26 09:33:01 -0700

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,28 +2,25 @@
 const list = document.querySelector("#toDoList");
 //function for a new li & checkbox
 function add() {
-  const inputToDo = document.getElementById('inputToDo');
+  const inputToDo = document.getElementById("inputToDo");
+  if (inputToDo.value === " ") return;
   const li = document.createElement("li");
   const textNode = document.createTextNode(inputToDo.value);
   const box = document.createElement("input");
   console.log("clicked");
   box.type = "checkbox";
-  li.addEventListener('click', removeLi);
+  li.addEventListener("click", removeLi);
   li.appendChild(box);
   li.appendChild(textNode);
   list.appendChild(li);
-  document.getElementById('inputToDo').value="";
+  document.getElementById("inputToDo").value = "";
 }
-//stopping to do item from being an empty to do item
-function emptyInput(){
-  if (inputToDo.value === " " ){
-alert("are you sure?");
-  }
- 
-}
-//removing li by checkbox
-function removeLi(e){
-  console.log('event target:', e.target);
+function removeLi(e) {
   const liElement = e.target.parentNode;
-  list.removeChild(liElement);
+  console.log("liElement");
+  if (e.target === liElement) {
+    box.style.textDecoration = "line-through";
+    list.removeChild(liElement);
+  }
 }
+
