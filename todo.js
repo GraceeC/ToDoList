b18a810324a35f7606eb5249c3bec95cd3d7549c
Grace C <51942103+GraceeC@users.noreply.github.com>
2019-10-27 08:48:57 -0700

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,25 +2,43 @@
 const list = document.querySelector("#toDoList");
 //function for a new li & checkbox
 function add() {
+  //getting input value
   const inputToDo = document.getElementById("inputToDo");
+  //stopping an empty input from being inserted
   if (inputToDo.value === " ") return;
+  //creating a new li element
   const li = document.createElement("li");
+  //ctreating text
   const textNode = document.createTextNode(inputToDo.value);
+  //creating a new box attribute
   const box = document.createElement("input");
   console.log("clicked");
   box.type = "checkbox";
+  //
   li.addEventListener("click", removeLi);
+  //appending a new checkbox for each item
   li.appendChild(box);
+  //appending an li for each item
   li.appendChild(textNode);
-  list.appendChild(li);
+  ulList.appendChild(li);
+  //clearing input
   document.getElementById("inputToDo").value = "";
 }
-function removeLi(e) {
-  const liElement = e.target.parentNode;
-  console.log("liElement");
-  if (e.target === liElement) {
-    box.style.textDecoration = "line-through";
+
+//event listener for strike through affect
+list.addEventListener("click", Strike);
+//function to strike item
+function Strike(e) {
+  const line = e.traget.nextSibiling;
+  if (e.targte.checked) {
+    line.style.textDecoration = "line-through";
+  }
+
+  // deletes li's
+  function removeLi(e) {
+    console.log("event target:", e.target);
+    const liElement = e.target.parentNode;
+    console.log("removeLi() function was run");
     list.removeChild(liElement);
   }
 }
-
