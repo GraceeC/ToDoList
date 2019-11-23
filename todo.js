f9a7a63609ac75d466f57676656ada8d02dbb5ca
Grace C <51942103+GraceeC@users.noreply.github.com>
2019-11-23 05:37:11 -0700

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,58 +1,64 @@
 //global scope for to do list
 const list = document.querySelector("#toDoList");
-//global scope for local storage
-const listItems =document.querySelector("#ulList");
+
+const listItems = document.querySelector("#ulList");
+ulList.addEventListener("click", function(e) {
+  console.log("box is clicked");
+  if(e.target.parentNode.style.textDecoration ==="line-through"){
+  } else{
+    e.target.parentNode.style.textDecoration ===" ";
+  }
+});
+
+const enterButton = document.getElementById('btnEnter');
+enterButton.addEventListener("click", function(e) {
+  add();
+});
+
+const inputToDo = document.getElementById("inputToDo");
+inputToDo.addEventListener("keyup",function(e){
+  if(event.keyCode === 13){
+    e.preventDefault();
+    add();
+  }
+});
 
 // function is ran when a new to do is entered
 function add() {
   //getting input value
-  const inputToDo = document.getElementById("inputToDo");
-  inputToDo.addEventListener("keyup",function(e){
-    if(event.keyCode === 13){
-      e.preventDefault();
-      document.getElementById("btnEnter").click();
-    }
-  });
+  const newLiItem = createListItemElement(inputToDo.value);
   //stopping input from entering an empty to do
   if (inputToDo.value.length === 0)
     return false;
-  //creating a new li
+  //adding a new checkbox for each item
+  ulList.appendChild(newLiItem);
+  //clearing input value after item has been submitted
+  document.getElementById("inputToDo").value = "";
+}
+function createListItemElement(inputToDoValue){
   const li = document.createElement("li");
   console.log("'li' created");
-  //creating text of new item
-  const textNode = document.createTextNode(inputToDo.value);
+   //creating text of new item
+   const textNode = document.createTextNode(inputToDoValue);
+   li.appendChild(textNode);
   //creating a new checkbox for each to do
   const box = document.createElement("input");
   console.log("clicked");
   box.type = "checkbox";
-  //btn to delete to do
-  const deleteBtn = document.createElement("input");
-  deleteBtn.setAttribute = ("type", "button");
-  deleteBtn.value = 'x';
-  deleteBtn.addEventListener('click', function (e) {
-    console.log("btn clicked");
-    li.style = 'display: none;';
-  });
-  //adding a new checkbox for each item
+
+  //adding a new checkbox
   li.appendChild(box);
-  ulList.appendChild(li);
-  li.appendChild(textNode);
-  li.appendChild(deleteBtn);
-  //clearing input value after item has been submitted
-  document.getElementById("inputToDo").value = "";
-  //local storage
-  
-}
-//event listener to strike out item
-ulList.addEventListener("click", Strike);
-//function to strike
-function Strike(e) {
-  line = document.getElementsByClassName("item");
-  console.log("box is clicked");
-  e.target.parentNode.style.textDecoration = "line-through";
-}
-function savedData(){
-localStorage.setItem('ulList', 'items');
-const value = localStorage.getItem(ulList);
-console.log(value);
-}
+
+   //btn to delete to do
+   const deleteBtn = document.createElement("input");
+   deleteBtn.setAttribute = ("type", "button");
+   deleteBtn.value = 'x';
+   deleteBtn.addEventListener('click', function(e){
+     console.log("btn clicked");
+     li.style = 'display: none;';
+   });
+   li.appendChild(deleteBtn);
+
+return li;
+
+  }
